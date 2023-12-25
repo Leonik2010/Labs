2abbc24fbcc31d222e8157ea5df55e0df580b8a7
Leonid Nikitin <145748576+Leonik2010@users.noreply.github.com>
2023-12-26 00:03:52 +0300

    Add files via upload

---
+++ opd/script.sh
@@ -0,0 +1,122 @@
+chmod -R 777 lab0
+rm -r lab0
+
+
+
+mkdir lab0
+cd lab0
+echo "satk=8 sdef=8 spd=6" > ivysaur4
+echo "satk=10 sdef=4
+spd=10" > sharpedo0
+mkdir totodile2
+cd totodile2
+mkdir ekans
+mkdir beheeyem
+cd ..
+mkdir trapinch3
+cd trapinch3
+mkdir drowzee
+mkdir haunter
+cd ..
+mkdir vileplume6
+cd vileplume6
+mkdir duosion
+mkdir elekid
+mkdir pidgey
+cd ..
+echo "Ходы After You Body Slam Block Covet Double-Edge
+Dinamicpunch Fire Punch Focus Punch Furry Cutter Gunk Shot Ice Punch
+Icy Wind Low Kick Mega Kick Mega Punch Mud-Slap Rock Climb Seismic
+Toss Shock Wave Sleep Talk Snore sucker Punch Thunderpunch Water Pulse
+Uproar" > slaking9
+cd totodile2
+echo "Развитые способности Rattled" > granbull
+cd ..
+cd trapinch3
+echo "Тип покемона
+Rock Bug" > armaldo
+cd ..
+cd vileplume6
+echo "Endeavor Giga Drain Gravity Iron Defense
+Iron Head Knock Off Magnet Rise Rock Climb Seed Bomb Sleep Talk Snore
+Stealth Rock Worry Seed" > ferrothorn
+echo "Тип Диеты Carnivore" > hydreigon
+cd ..
+
+
+
+echo "\n\n Раздачав прав"
+chmod 440 ivysaur4
+chmod 624 sharpedo0
+chmod 620 slaking9
+chmod 700 totodile2
+cd totodile2
+chmod 755 ekans
+chmod 551 beheeyem
+chmod 666 granbull
+cd ..
+cd trapinch3
+chmod 024 armaldo
+chmod 733 drowzee
+chmod 375 haunter
+cd ..
+chmod 753 trapinch3
+chmod 500 vileplume6
+cd vileplume6
+chmod 440 ferrothorn
+chmod 006 hydreigon
+chmod 736 elekid
+chmod 375 pidgey
+chmod 373 duosion
+cd ..
+
+
+
+echo "\n\n Создание ссылок"
+cd trapinch3
+chmod 400 haunter
+chmod 400 armaldo
+cd ..
+cd vileplume6
+chmod 400 hydreigon
+cd ..
+
+ln -s /home/studs/s409236/lab0/trapinch3 /home/studs/s409236/lab0/Copy_29
+cp -R /home/studs/s409236/lab0/trapinch3 /home/studs/s409236/lab0/vileplume6/elekid
+cp /home/studs/s409236/lab0/ivysaur4 /home/studs/s409236/lab0/vileplume6/duosion
+ln -s /home/studs/s409236/lab0/sharpedo0 /home/studs/s409236/lab0/trapinch3/armaldosharpedo
+cat /home/studs/s409236/lab0/vileplume6/ferrothorn /home/studs/s409236/lab0/vileplume6/hydreigon > slaking9_88
+ln /home/studs/s409236/lab0/sharpedo0 /home/studs/s409236/lab0/totodile2/grandbullsharpedo
+
+
+cd trapinch3
+chmod 375 haunter
+chmod 024 armaldo
+cd ..
+cd vileplume6
+chmod 373 duosion
+cd ..
+
+
+
+echo "\n4.1"
+wc -m $(ls -d */h*) | sort -n
+echo "\n4.2"
+ls -l **/h* | head -n 2 2>&1 | sort -nk 2
+echo "\n4.3"
+grep -inh "e$" --directories="skip" trapinch3/*
+echo "\n4.4"
+grep -hi --directories="skip" "spd" **/armaldo **/ferrothorn
+echo "\n4.5"
+ls -l **/e* | head -n 4 2>/tmp/er | sort -rMk 7
+echo "\n4.6"
+ls -l **/a* 2>&1 | sort -nk 2
+
+
+
+echo "\n\n Удаление"
+rm slaking9
+rm /vileplume6/ferrothorn
+rm Copy_29
+
+
